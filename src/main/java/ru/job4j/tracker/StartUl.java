af1a2eda0e6f947f1237760cf9a112a0b7b76536
Oleg <nikishin-o.a@mail.ru>
2023-07-23 10:36:59 +0500

    8. Date. Отображение даты. [#310017  [#310017 #431530]]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -4,10 +4,6 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUl {
 
-    public StartUl() {
-
-    }
-
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
